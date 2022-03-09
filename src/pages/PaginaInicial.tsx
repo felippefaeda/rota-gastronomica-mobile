e59546208dba7e8427b947d258d98e74d6ab2f5c
felippefaeda <felippefaeda@gmail.com>
2022-03-09 16:26:51 -0300

    Get Json s3 aws

--- src/pages/PaginaInicial.tsx
+++ src/pages/PaginaInicial.tsx
@@ -75,7 +75,9 @@ export function PaginaInicial() {
             }
         }
 
-        getJsonServer();
+        if (config.SEARCH_TYPE == 'LOCAL'){
+            getJsonServer();
+        }        
     }, []);
 
     return (
